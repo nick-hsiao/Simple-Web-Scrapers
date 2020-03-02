6e991f885a4a6b9d1b24d396a1daf4b53409f22f
Nick Hsiao <hsiao.nicholas@gmail.com>
2020-03-01 19:37:01 -0800

    location scraped

--- web_scraper.py
+++ web_scraper.py
@@ -6,14 +6,10 @@ source = requests.get('http://www.sjsu.edu/').text
 
 soup = BeautifulSoup(source,'lxml')
 
-#print(soup.prettify())
-
-
 for event in soup.find_all('a',class_='o-calendar__event'):
 	
 	event_title = event.find('p',class_='o-calendar__event-name u-bar u-bar--light u-bar--sjsu-gold').text
-	#print(event_title)
-	#print(event.prettify())
+
 	try:
 		event_link = event['href']
 	except Exception as e:
@@ -23,6 +19,13 @@ for event in soup.find_all('a',class_='o-calendar__event'):
 	event_day = event.find('span',class_='o-calendar__day').text
 
 	event_time = event.find('span',class_='o-calendar__detail u-icon u-icon--clock u-color--sjsu-gold').text
-	event_time = event_time.replace(' ','').replace('Time:','').strip()
+	event_time = event_time.replace('Time:','').strip()
 
-	print(event_title,event_month,event_day,event_time)
+	event_location = event.find('span',class_='o-calendar__detail u-icon u-icon--location u-color--sjsu-gold').text
+	event_location = event_location.replace('Location:','').strip()
+	
+	print(event_title)
+	print(event_month,event_day, event_time)
+	print(event_location)
+	print(event_link)
+	print()
