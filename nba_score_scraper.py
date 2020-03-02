f439fd72fb7bc421032bc40763cab87aeea1be26
Nick Hsiao <hsiao.nicholas@gmail.com>
2020-03-01 21:55:51 -0800

    winning and losing teams with score and player of the game finished

--- nba_score_scraper.py
+++ nba_score_scraper.py
@@ -11,18 +11,38 @@ for team in soup.find_all('div',class_='team__list'):
 	if team.text == 'Los Angeles Lakers':
 		nba_teams.append('LA Lakers')
 	nba_teams.append(team.text)
-	
+
 source = requests.get('https://www.basketball-reference.com/boxscores/').text
 soup = BeautifulSoup(source,'lxml')
 
-todays_games = soup.find('div',class_='game_summary expanded nohover')
-print(todays_games.prettify())
+for game in soup.find_all('div',class_='game_summary expanded nohover'):
+	#print(game.prettify())
 
-winner = todays_games.find('tr',class_='winner')
-#print(winner.a.text)
+	winner = game.find('tr',class_='winner')
+	#print(winner.a.text)
+	winner_score = winner.find('td',class_='right').text
 
-for team in nba_teams:
-	if winner.a.text in team:
-		winner = team
-		break
-print(winner)
+
+	for team in nba_teams:
+		if winner.a.text in team:
+			winner = team
+			break
+	
+	print(winner,winner_score)
+	loser = game.find('tr',class_='loser')
+	loser_score = loser.find('td',class_='right').text
+	
+	for team in nba_teams:
+		if loser.a.text in team:
+			loser = team
+			break
+
+	print(loser,loser_score)
+
+	player = game.find('table',class_='stats')
+	player_name = player.a.text
+	player_score = player.find('td',class_='right').text
+	print(player_name,player_score,'\n')
+
+	
+
