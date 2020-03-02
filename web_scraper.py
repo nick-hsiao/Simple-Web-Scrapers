2cb987b49ad04174bf67402bf2ada0ae77852f1a
Nick Hsiao <hsiao.nicholas@gmail.com>
2020-03-01 19:32:05 -0800

    time scrape

--- web_scraper.py
+++ web_scraper.py
@@ -10,9 +10,9 @@ soup = BeautifulSoup(source,'lxml')
 
 
 for event in soup.find_all('a',class_='o-calendar__event'):
-
+	
 	event_title = event.find('p',class_='o-calendar__event-name u-bar u-bar--light u-bar--sjsu-gold').text
-	print(event_title)
+	#print(event_title)
 	#print(event.prettify())
 	try:
 		event_link = event['href']
@@ -22,5 +22,7 @@ for event in soup.find_all('a',class_='o-calendar__event'):
 	event_month = event.find('span',class_='o-calendar__month').text
 	event_day = event.find('span',class_='o-calendar__day').text
 
+	event_time = event.find('span',class_='o-calendar__detail u-icon u-icon--clock u-color--sjsu-gold').text
+	event_time = event_time.replace(' ','').replace('Time:','').strip()
 
-	print(event_month, event_day)
+	print(event_title,event_month,event_day,event_time)
